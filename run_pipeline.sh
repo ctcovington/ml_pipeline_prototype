a9ab64fc8f495c223c6cf6965271c4c30b57a5a3
Christian Covington <christian.t.covington@gmail.com>
2018-07-31 18:25:23 -0400

    add conda environment

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-snakemake --dag | dot -Tsvg > dag.svg # create directed acyclic graph showing workflow
+
+# either create or load conda environment
+if conda info --envs | grep -q "ml_pipeline"; then
+    source activate "ml_pipeline"
+else
+    conda env create -f conda_env.yaml
+fi
+
+# create directed acyclic graph showing workflow
+snakemake --dag | dot -Tsvg > dag.svg
+
+# run pipeline
 snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" & # run pipeline
