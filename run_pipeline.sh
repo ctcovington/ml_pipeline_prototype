153ec0f82b63c0cd58dac62cf9f4d1aac56a562b
Christian Covington <christian.t.covington@gmail.com>
2018-08-14 19:44:49 -0400

    source conda env upfront

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,11 +5,14 @@ if ! conda info --envs | grep -wq "ml_pipeline"; then
     conda env create -f=conda_env.yaml
 fi
 
-# # load conda environment
-# source activate "ml_pipeline"
+# load conda environment
+source activate "ml_pipeline"
 
 # create directed acyclic graph showing workflow
 snakemake --dag | dot -Tsvg > dag.svg
 
 # run pipeline
 snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" & # run pipeline
+
+# deactivate source environment
+source deactivate
