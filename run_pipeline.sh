48fefcb372c688d927de067477f19e44064ee5ca
Christian Covington <christian.t.covington@gmail.com>
2018-08-15 11:05:37 -0400

    remove extraneous comment

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -12,7 +12,7 @@ source activate "ml_pipeline"
 snakemake --dag | dot -Tsvg > dag.svg
 
 # run pipeline
-nohup snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" > snakemake.log 2>&1 & # run pipeline
+nohup snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" > snakemake.log 2>&1 &
 
 # deactivate conda environment
 source deactivate
