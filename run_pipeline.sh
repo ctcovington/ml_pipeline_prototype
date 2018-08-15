86b8f8c55ece8baa134df58fd82fb667dd558918
Christian Covington <christian.t.covington@gmail.com>
2018-08-15 09:51:15 -0400

    run via nohup and log snakemake commands

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -12,7 +12,7 @@ source activate "ml_pipeline"
 snakemake --dag | dot -Tsvg > dag.svg
 
 # run pipeline
-snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" & # run pipeline
+nohup snakemake -j 20 --cluster-config cluster.json --cluster "bsub -K -q {cluster.queue} -J {cluster.name} -M {cluster.memory} -R {cluster.resources}" > snakemake.log 2>&1 & # run pipeline
 
-# deactivate source environment
+# deactivate conda environment
 source deactivate
